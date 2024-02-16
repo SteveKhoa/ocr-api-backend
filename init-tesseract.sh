62b8c84ea6f4b24efdeb33734887e1cc8346e285
Khoa LTN <vietnamkhoale@gmail.com>
2024-02-16 11:49:33 +0700

    refactor: rename init.sh to init-tesseract.sh

---
+++ init-tesseract.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This script handily initializes Tesseract container
+# This script will build the container again.
+
+cd services/tesseract
+
+docker rm dev-tesseract
+docker rmi img-dev-tesseract
+
+docker build -t img-dev-tesseract .
+docker create --publish 22:22 \
+    --name dev-tesseract \
+    --mount type=bind,source="$(pwd)",target=/home/ubuntu/tesseract \
+    img-dev-tesseract
+docker start dev-tesseract
