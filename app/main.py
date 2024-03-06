eddd68dad2b8b5bd455e55ea1073509c90078fa6
Khoa LTN <vietnamkhoale@gmail.com>
2024-03-06 13:53:51 +0000

    feat: add greetings API end-point

--- app/main.py
+++ app/main.py
@@ -1,8 +1,14 @@
 from fastapi import FastAPI
 from .auth import router as auth_router
 from .ocr import router as ocr_router
+from app.response import Message
 
 app = FastAPI()
 
 app.include_router(auth_router.auth_router)
 app.include_router(ocr_router.ocr_router)
+
+
+@app.get("/")
+async def greetings():
+    return Message("Greetings! Thanks for using OCR-API.")
