3aaa38074e7ede4f666615f77e2ba15a4e387905
Khoa LTN <vietnamkhoale@gmail.com>
2024-03-12 16:27:52 +0000

    refactor: linting run.py

--- run.py
+++ run.py
@@ -7,10 +7,16 @@ import os
 environ = os.environ.get("ENVIRON")
 
 if __name__ == "__main__":
-    
+
     match environ:
         case "dev":
-            uvicorn.run("app.main:app", reload=True)
+            uvicorn.run(
+                "app.main:app",
+                host="127.0.0.1",
+                timeout_keep_alive=0,
+                log_level="trace",
+                reload=False,
+            )
         case "prod":
             print("Not implemented....sorry")
             print("Process stopped...")
