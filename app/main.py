f155603590f5584b71da97f007a8f916e4b98eed
Khoa LTN <vietnamkhoale@gmail.com>
2024-03-14 15:25:53 +0000

    feat: add app lifespan action (on STARTUP/SHUTDOWN)

--- app/main.py
+++ app/main.py
@@ -4,8 +4,23 @@ from .auth import router as auth_router
 from .ocr import router as ocr_router
 from app._responses import Message
 from app._exceptions import reponse_http_exception
+import contextlib
+from app.db.script import initialize_database
 
-app = FastAPI()
+
+@contextlib.asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Pre-run and After-run of FastAPI app"""
+
+    # On startup
+    initialize_database()
+
+    yield
+    # On shutdown
+    pass
+
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(auth_router.auth_router)
 app.include_router(ocr_router.ocr_router)
