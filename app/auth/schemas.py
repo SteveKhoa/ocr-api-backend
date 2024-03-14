e760d9ff6ce7cb07c02a9de6c46342b6a5b5fed9
Khoa LTN <vietnamkhoale@gmail.com>
2024-03-14 15:29:54 +0000

    fix: reimplements User and VerifiedUser pydantic schema (not finished)

--- app/auth/schemas.py
+++ app/auth/schemas.py
@@ -3,22 +3,45 @@ Pydantic schemas for Authentication-Authorization module
 """
 
 from pydantic import BaseModel
+from pydantic import Field
+from pydantic import field_validator
+from typing import Annotated
+import app._exceptions
+import re
+
+
+MAX_USERNAME_LENGTH = 32
 
 
 class User(BaseModel):
     username: str
-    email: str | None = None
-    full_name: str | None = None
-    disabled: bool | None = None
+    password: str | None = None
+
+    @field_validator("username")
+    def validate_username(cls, username):
+        if len(username) <= 0:
+            raise app._exceptions.InvalidData("username must not be empty.")
+
+        if len(username) > MAX_USERNAME_LENGTH:
+            raise app._exceptions.InvalidData("username too long (> 32 characters).")
+
+        USERNAME_PATTERN = r"^[_0-9a-zA-Z]+$"
+        if len(re.findall(pattern=USERNAME_PATTERN, string=username)) <= 0:
+            raise app._exceptions.InvalidData(
+                f"username must be comply pattern [{USERNAME_PATTERN}]."
+            )
+
+        return username  # pass all validations
 
 
-class UserInDB(User):
-    hashed_password: str
-
-
-# Currently duplicate but temporarily ignore it.
-# In the future I want to model the schemas based
-# on the specific domain, not copying and pasting
-# from FastAPI docs
 class VerifiedUser(User):
-    pass
+    access_token: str
+
+    @field_validator("access_token")
+    def validate_access_token(cls, access_token):
+
+        JWT_PATTERN = r"(^[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+\.[A-Za-z0-9-_]+$)"
+        if len(re.findall(pattern=JWT_PATTERN)) <= 0:
+            raise app._exceptions.InvalidData("invalid JWT pattern.")
+
+        return access_token  # pass all validations
