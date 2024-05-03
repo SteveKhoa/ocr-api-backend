3fc71494360e63f94a0221fecf528e5136312efc
Khoa LTN <vietnamkhoale@gmail.com>
2024-05-03 15:02:31 +0000

    chore: divide responses in responses.py into 3 categories for JSend compliance

--- app/responses.py
+++ app/responses.py
@@ -1,5 +1,16 @@
 """
-Common JSON Response formats for OCR-API
+Common JSON Response format for OCR-API.
+
+The format is JSend-compliance. See https://github.com/omniti-labs/jsend.
+
+Success
+    *Required keys*: status, data
+
+Fail
+    *Required keys*: status, data
+
+Error
+    *Required keys*: status, code, message, data
 """
 
 from typing import Any
@@ -7,52 +18,62 @@ from fastapi import status as http_status
 import fastapi.responses
 
 
-class Response:
-    """Response JSON
-
-    The format is JSend-compliance. See https://github.com/omniti-labs/jsend.
-
-    ## Schema
-    #### Success
-
-    *Required keys*: status, data
-
-    #### Fail
-
-    *Required keys*: status, data
-
-    #### Error
-
-    *Required keys*: status, code, message, data
-    """
-
+class BaseResponse:
     async def __call__(self, scope, receive, send) -> None:
-        response = fastapi.responses.JSONResponse(
-            self.to_json(), self.status
-        )
+        response = fastapi.responses.JSONResponse(self.to_json(), self.status)
 
         await response(scope, receive, send)
+
+
+class SuccessResponse(BaseResponse):
+    def __init__(self, status: int = None, data: Any = None):
+        # Define interface explicitly, even when these parameters are
+        # not in use, to clearly declare what the response would contain
+        # (to be JSend compliance)
+        pass
 
     def to_json(self):
         return {"status": http_status.HTTP_200_OK, "data": self.data}
 
 
-class Text(Response):
+class Text(SuccessResponse):
     """Text response"""
 
     def __init__(self, text: str):
         self.data = {"text": text}
 
 
-class Collection(Response):
+class Collection(SuccessResponse):
     """A list of Any"""
 
     def __init__(self, collection: list[Any]):
         self.data = {"collection": [item for item in collection]}
 
 
-class AccessToken(Response):
+class AccessToken(SuccessResponse):
     """String representing access token"""
 
     def __init__(self, token: str):
         self.data = {"access_token": token}
+
+
+class FailResponse(BaseResponse):
+    def __init__(self, status: int, data: Any = None):
+        self.status = status
+
+    def to_json(self):
+        return {"status": self.status, "data": self.data}
+
+
+class ErrorResponse(BaseResponse):
+    def __init__(self, code: int, message: int, data: Any = None):
+        self.status = code
+        self.message = message
+
+    def to_json(self):
+        return {
+            "status": self.status,
+            "code": "",  # omit for now
+            "message": self.message,
+            "data": "",  # omit for now
+        }
